816d6aa5c24498e75809844fbca59f4d33222225
Kiu L <zqdummy@gmail.com>
2020-11-25 16:48:14 -0800

    code player selection of grid

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 const player1 = "red";
 const player2 = "blue";
 
+let currentplayer = "red";
+
 // -- generate an array of button element references
 const buttonsRef = document.querySelectorAll("button");
 
@@ -11,6 +13,21 @@ const buttonsRef = document.querySelectorAll("button");
 function clickButton(e) {
   let clickButtonRef = e.target;
   clickButtonRef.setAttribute("disabled", "");
+
+  if (currentplayer === "red") {
+    clickButtonRef.setAttribute("style", "background-color:red");
+    currentplayer = "blue";
+    checkGameStatus();
+  } else {
+    clickButtonRef.setAttribute("style", "background-color:blue");
+    currentplayer = "red";
+    checkGameStatus();
+  }
+}
+
+// -- Functino runs each time a selection is made to determine if there is a winner
+function checkGameStatus() {
+  console.log("checking");
 }
 
 // -- add an event listener to all the buttons
