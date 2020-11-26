38ec02282f67511ed5159e29c161b724f69b5f30
Kiu L <zqdummy@gmail.com>
2020-11-25 16:29:09 -0800

    add code to select and disable buttons

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-alert("Let the game start");
+// -- BUTTONS in this script each represent a quare on the tic tac toe grid
+
+// -- global variables
+const player1 = "red";
+const player2 = "blue";
+
+// -- generate an array of button element references
+const buttonsRef = document.querySelectorAll("button");
+
+// -- when a button is click, do the following
+function clickButton(e) {
+  let clickButtonRef = e.target;
+  clickButtonRef.setAttribute("disabled", "");
+}
+
+// -- add an event listener to all the buttons
+buttonsRef.forEach((element) => element.addEventListener("click", clickButton));
